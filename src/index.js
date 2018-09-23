709c8b91af5d959df6192a5ddab4dd15168d1cd8
willybc <willybc06@gmail.com>
2018-09-23 16:42:19 -0300

    Estilos

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import Media from './playlist/componentes/media.js';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 
--- src/playlist/componentes/media.js
+++ src/playlist/componentes/media.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import image from '../../assets/bitcoin.jpg';
+import './media.css';
 
 class Media extends Component {
     render(){
+    const styles = {
+        container: {
+            fontSize: 14,
+            /*background: 'red',*/
+            color: '#44546b',
+            cursor: 'pointer',
+            width: 260,
+            border: '1px solid red'
+        }
+    }
+
+    console.log(image);
         return(
-            <div>
+            <div className="Media">
                 <div>
-                    <img src="" alt="" />
+                <img
+                 src={image}
+                 alt="" 
+                 width={260}
+                 height={160}
+                />
                     <h3>¿Por que React</h3>
                     <p>Willy Bardales</p>
                 </div>
