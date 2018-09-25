f982dc25a09354ae8daaaff02374b452e6a1a7e1
willybc <willybc06@gmail.com>
2018-09-24 21:29:46 -0300

    Implementando bootstrap y añadiendo componente root

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to my new proyect</h1>
+        </header>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
+//Componentes
 import Playlist from './playlist/componentes/playlist';
+import App from './App';
+//Api
 import data from './api.json';
+
+//Bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 
 import './index.css';
 //import registerServiceWorker from './registerServiceWorker';
 
+//constantes
 const app = document.getElementById('root');
+const body = document.getElementById('body');
 
-render(<Playlist data={data} />, app);
+render(<App />, app);
+render(<Playlist data={data} />, body);
